25ece48a616c903293e3e4b16fbff892be007099
Priya Kumari <chaudhary21priya@gmail.com>
2025-09-13 14:21:46 +0530

    feat(auth): implement authentication

---
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import login from '../../../../public/login.jpg'
+
+export default function Page() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2'>
+        <div>
+            <Image src={login} alt='login' width={500} height={500} 
+            className='w-full h-screen object-contain'/>
+        </div>
+        <div className='flex justify-center items-center'>
+            <SignUp />
+        </div>
+    </div>
+  )
+}
